a85a80d8519aeb2e44817367c5718111e1b97eaf
kevinmarquez1367 <kevinmarquez1367@gmail.com>
2021-02-13 19:31:53 +0100

    Añadida funcion 1

---
+++ src/JavaProject/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("FUNCIONALIDAD BASICA");
+        method1();
+    }
+    public static void method1() {
+        System.out.println("Metodo nuevo 1");
+}
+    
+}
