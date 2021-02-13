180e7cb3ac75e03eb7a977bd166656f7cf0c5031
Jeiny <jeiny206@gmail.com>
2021-02-13 23:48:52 +0100

    testing mwthod

--- src/JavaProject/src/Main.java
+++ src/JavaProject/src/Main.java
@@ -3,9 +3,14 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("FUNCIONALIDAD BASICA");
         method1();
+	method2();
     }
+
     public static void method1() {
         System.out.println("Metodo nuevo 1");
-}
-    
+    }
+
+    public static void method2() {
+        System.out.println("testing method 2");
+    }
 }
